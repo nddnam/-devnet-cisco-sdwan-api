1df2ed324d936a4b7793ee373dc70bf93e3c84ac
Nam Nguyen <info@itbase.tv>
2024-10-16 11:10:33 +0700

    add list of sample output

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ device = sapi.DEVICES(header)
 #
 ##DEVICE LIST
 dev_list = device.get_device_list()
-#sapi.CSV_EXPORT(dev_list, "device-list.csv")
+sapi.CSV_EXPORT(dev_list, "device-list.csv")
 #
 
 ##CONTROL CONNECTION CHECK FOR EACH
@@ -33,16 +33,16 @@ dev_list = device.get_device_list()
 #sapi.CSV_EXPORT(control_connections, "10.10.10.12-control-check.csv")
 
 ##CONTROL CONNECTION CHECK FOR ALL 2024-10-14
-for dev in dev_list:
+#for dev in dev_list:
 #    #print(f"{dev['system-ip']}")
-    control_connections = device.control_check(dev['system-ip'])
-    sapi.CSV_EXPORT(control_connections, f"{dev['system-ip']}-control-connections.csv")
+ #   control_connections = device.control_check(dev['system-ip'])
+ #   sapi.CSV_EXPORT(control_connections, f"{dev['system-ip']}-control-connections.csv")
 
 ##BFD SESSIONS CHECK FOR ALL 2024-10-14
 #system_ip = '10.0.1.1'
-for dev in dev_list:
-    bfd_sessions = device.get_bfd_sessions(dev['system-ip'])
-    sapi.CSV_EXPORT(bfd_sessions, f"{dev['system-ip']}-bfd-sessions.csv")
+#for dev in dev_list:
+ #   bfd_sessions = device.get_bfd_sessions(dev['system-ip'])
+  #  sapi.CSV_EXPORT(bfd_sessions, f"{dev['system-ip']}-bfd-sessions.csv")
 
 #TEMPLATE ADMIN
 #template = sapi.TEMPLATE(header)
